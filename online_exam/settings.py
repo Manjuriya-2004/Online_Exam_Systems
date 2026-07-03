aad1bc0c87549fd9038cded016fc1904a2b347fe
easywe <rmanjuriyamanjuriya@gmail.com>
2026-07-03 16:35:50 +0530

    email config

--- online_exam/settings.py
+++ online_exam/settings.py
@@ -119,7 +119,7 @@ LOGIN_URL = 'login'
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+'''EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
@@ -129,3 +129,18 @@ EMAIL_HOST_USER = 'karthiindra2000@gmail.com'
 EMAIL_HOST_PASSWORD = 'oyhf xvju pxat befn'
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+'''
+
+import os
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
